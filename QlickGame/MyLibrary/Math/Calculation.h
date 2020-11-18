7be19fb97f8f590a3663fbb1e737da1026741a37
kunimatsutaiyo <wolffang000@gmail.com>
2020-11-18 10:02:56 +0900

    足し算関数を追加しました。

---
+++ QlickGame/MyLibrary/Math/Calculation.h
@@ -0,0 +1,29 @@
+#ifndef CALCULATION_H_
+#define CALCULATION_H_
+
+class Calculation
+{
+public:
+	Calculation()
+	{
+
+	}
+	~Calculation()
+	{
+
+	}
+
+public:
+	int Add(int a_, int b_)
+	{
+		return (a_ + b_);
+	}
+
+
+private:
+
+
+
+};
+
+#endif
